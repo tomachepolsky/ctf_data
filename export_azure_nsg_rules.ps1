62adba98212f137fa480ac120427cde1326fa354
tomachepolsky <9163229+tomachepolsky@users.noreply.github.com>
2019-10-07 18:20:41 +0100

    Create export_azure_nsg_rules.ps1

---
+++ export_azure_nsg_rules.ps1
@@ -0,0 +1,12 @@
+$nsgs = Get-AzureRmNetworkSecurityGroup
+$exportPath = '.'
+
+#backup nsgs to csv
+Foreach ($nsg in $nsgs) {
+    New-Item -ItemType file -Path "$exportPath\$($nsg.Name).csv" -Force
+    $nsgRules = $nsg.SecurityRules
+    foreach ($nsgRule in $nsgRules) {
+        $nsgRule | Select-Object Name,Description,Priority,@{Name=’SourceAddressPrefix’;Expression={[string]::join(“,”, ($_.SourceAddressPrefix))}},@{Name=’SourcePortRange’;Expression={[string]::join(“,”, ($_.SourcePortRange))}},@{Name=’DestinationAddressPrefix’;Expression={[string]::join(“,”, ($_.DestinationAddressPrefix))}},@{Name=’DestinationPortRange’;Expression={[string]::join(“,”, ($_.DestinationPortRange))}},Protocol,Access,Direction `
+        | Export-Csv "$exportPath\$($nsg.Name).csv" -NoTypeInformation -Encoding ASCII -Append
+    }
+}
